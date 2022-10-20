ecb636f35d95a87d50c01cb61d2999f883355bc7
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-10-20 18:35:06 +0900

    UserDapTest

---
+++ src/test/java/com/likelion/domain/UserDaoTest.java
@@ -0,0 +1,83 @@
+package com.likelion.domain;
+
+import com.likelion.dao.UserDao;
+import com.likelion.dao.UserDaoFactory;
+import com.likelion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
+class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    UserDao userDao;
+
+    User user1;
+
+    User user2;
+
+    User user3;
+
+
+    @BeforeEach
+    void setUp() {
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+        this.user1 = new User("1", "kyeongrok", "1123");
+        this.user2 = new User("2", "kyeonghwan", "1234");
+        this.user3 = new User("3", "sujin", "4321");
+    }
+
+    @Test
+    void addAndGet() throws SQLException {
+        User user1 = new User("1", "kyeongrok","1123");
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        String id = "29";
+        userDao.add(new User(id, "EternityHwan", "1234"));
+        assertEquals(1, userDao.getCount());
+        User user = userDao.findById(id);
+
+        assertEquals("EternityHwan", user.getName());
+        assertEquals("1234", user.getPassword());
+    }
+
+    @Test
+    void count() throws SQLException {
+        /*
+        User user1 = new User("1", "kyeongrok", "1123");
+        User user2 = new User("2", "kyeonghwan", "1234");
+        User user3 = new User("3", "sujin", "4321");
+        */
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+    }
+
+    @Test
+    void findById() {
+        assertThrows(EmptyResultDataAccessException.class,() -> {
+           userDao.findById("30");
+        });
+    }
+}
